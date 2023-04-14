f77a34f7e0ae1d1664a3beb3f86062ff8ed61730
Max <you@example.com>
2023-04-14 16:05:51 +0700

    inst small-clone

---
+++ fast-company/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Users from './components/users';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Users />
+    </div>
+  );
+}
+
+export default App;
