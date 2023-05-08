01cd145119cd595266c10eb7ee5da047ed8df883
Max <you@example.com>
2023-05-08 17:06:20 +0700

    update

---
+++ fast-company/src/app/components/users.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+import Pagination from "./pagination";
+import User from "./user";
+import {paginate} from "../utils/paginate.js"; 
+
+const Users = ({ users, ...rest }) => {
+    const count = users.length;
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+    }
+    const userCrop = paginate(users, currentPage, pageSize);
+    return (
+        <>
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userCrop.map((user) => (
+                            <User key={user._id} {...rest} {...user} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination itemsCount={count} pageSize={pageSize} currentPage={currentPage} onPageChange={handlePageChange}/>
+        </>
+    );
+};
+
+export default Users;
